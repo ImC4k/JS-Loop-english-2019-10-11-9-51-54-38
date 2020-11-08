9f831edf88bf345cb8c7be2e9788be53d0bf97bb
ImC4k <imc4k@ImC4k-main.local>
2020-11-08 20:05:42 +0800

    conventional loop

--- number/index.js
+++ number/index.js
@@ -1,3 +1,8 @@
 const arrLength = 20;
 var a = [...Array(arrLength + 1).keys()];
-a.forEach(item => (item % 2) == 0? console.log(`${item} is even number.`) : console.log(`${item} is odd number.`));
+// a.forEach(item => (item % 2) == 0? console.log(`${item} is even number.`) : console.log(`${item} is odd number.`));
+
+for (var i = 0; i < arrLength; ++i) {
+    if (a[i] % 2 == 0) console.log(`${a[i]} is even number.`);
+    else console.log(`${a[i]} is odd number.`);
+} 
